b69a485beecd63330835d02d009e3a4aea5fe349
Hanjin <32667737+hanzyn09@users.noreply.github.com>
2021-03-18 14:21:30 +0900

    Update operation.sol

--- operation.sol
+++ operation.sol
@@ -1,14 +1,20 @@
 pragma solidity ^0.4.11;
 
 contract Example005 {
-  function operation() returns (uint result) {
-    
+  function sum () returns (uint result) {
     result = sum(11,5);
-    result1 = multiple(11,5);
-    result2 = subtract(11,5);
-    result3 = division(11,5);
-    
   }
+  
+  function multiple () returns (uint result1) {
+    result1 = multiple(11,5);
+  }
+  function subtract () returns (uint result2) {
+    result2 = subtract(11,5);
+  }
+  function division () returns (uint result3) {
+     result3 = division(11,5);
+  }
+  
 
   function sum(uint a, uint b) returns (uint) {
       uint result = a + b;
